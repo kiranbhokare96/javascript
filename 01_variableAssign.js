a8577af0dc3e908205b436251ef744f60f5c1344
bhokarekiran <144211378+bhokarekiran@users.noreply.github.com>
2023-12-26 15:07:33 +0530

    File using Variable
    File using Variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,26 @@
+var FullName="Kiran Bhokare";
+console.log("FullName:",FullName);
+myFullName="Kiran Sanjay Bhokare";
+console.log("FullName:",myFullName);
+var form="Form";
+console.log("-----Form-----");
+console.log("I Love Only JS");
+var myName="Kiran";
+console.log("First Name:",myName);
+myName="Bhokare";
+console.log("Last Name:",myName);
+var age="27";
+console.log("Age:",age);
+var address="Pune";
+console.log("Address:",address);
+var pincode="424001";
+console.log("Pincode:",pincode);
+var gender="Female";
+console.log("Gender:",gender);
+var Maritalstatus="Married";
+console.log("Marital Status:",Maritalstatus);
+
+
+
+
+
---
+++ helloWord.js
@@ -0,0 +1,10 @@
+
+
+
+var myName="Kiran";
+console.log(myName);
+console.log("First'Name:",myName);
+myName="Kiran Bhokare";
+console.log("Full Name:",myName);
+var age="32";
+console.log("Age=",age);
